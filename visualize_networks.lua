929c15179bdc1b048848fe6565ac99038e6e4cff
Quinn Jarrell <qjarrell@gosynapsify.com>
2015-03-15 23:34:52 -0500

    .

--- visualize_networks.lua
+++ visualize_networks.lua
@@ -104,11 +104,11 @@ for network_name in lfs.dir(base_directory) do
       local images = {}
 
       for _, dataset_type in ipairs(dataset_types) do
-        print(lfs.attributes(base_directory .. '/tmp'.. base_directory .. "/networks" .. "/my_first_dcign/"..dataset_type))
-        if lfs.attributes(base_directory .. '/tmp'.. base_directory .. "/networks" .. "/my_first_dcign/"..dataset_type) ~= nil then
-          local last_epoch = lastepochnum('./tmp'.. base_directory .. "/networks" .. "/my_first_dcign/"..dataset_type)
+        print(lfs.attributes(base_directory .. '/tmp'.. base_directory .. "/my_first_dcign/"..dataset_type))
+        if lfs.attributes(base_directory .. '/tmp'.. base_directory .. "/my_first_dcign/"..dataset_type) ~= nil then
+          local last_epoch = lastepochnum(base_directory .. '/tmp'.. base_directory .. "/my_first_dcign/"..dataset_type)
           local reconstruction_gt = torch.load(base_directory .. "/" .. network_name .. '/CNN_DATASET/th_'..dataset_type..'/FT_test/batch' .. id)
-          local preds = torch.load("./tmp/mount/dc-ign/networks/my_first_dcign/"..dataset_type.."/epoch_"..last_epoch..'/preds' ..id)
+          local preds = torch.load(base_directory .. "/tmp/mount/dc-ign/networks/my_first_dcign/"..dataset_type.."/epoch_"..last_epoch..'/preds' ..id)
           print "hi"
           print(preds)
           for i=1, preds:size()[1] do

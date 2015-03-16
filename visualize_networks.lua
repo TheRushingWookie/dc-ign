88f5d1c65e4796fec1bc204c33b909a8ec3c6880
Quinn Jarrell <qjarrell@gosynapsify.com>
2015-03-15 23:23:15 -0500

    .

--- visualize_networks.lua
+++ visualize_networks.lua
@@ -94,7 +94,7 @@ end
 
 
 ---------------------- RECONSTRUCTION ----------------------
-local id=1
+local id=4
 for network_name in lfs.dir(base_directory) do
   local network_path = base_directory .. '/' .. network_name
   -- print(network_path)
@@ -108,7 +108,7 @@ for network_name in lfs.dir(base_directory) do
 
           local reconstruction_gt = torch.load(base_directory .. "/" .. network_name .. '/CNN_DATASET/th_'..dataset_type..'/FT_test/batch' .. id)
           local preds = torch.load("./tmp/mount/dc-ign/networks/my_first_dcign/"..dataset_type.."/epoch_"..last_epoch..'/preds' ..id)
-
+          print("./tmp/mount/dc-ign/networks/my_first_dcign/"..dataset_type.."/epoch_"..last_epoch..'/preds' ..id)
           for i=1, preds:size()[1] do
             print("hi")
             local image_row = {}
